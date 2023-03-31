5bc0faf7b3a50e48bab8ef642aa84f0028fff710
njaffe <noah0m0jaffe@gmail.com>
2023-03-31 08:50:45 -0400

    update limits and comments
    updated limits so that it hopefully works first time every time

--- runme.bat
+++ runme.bat
@@ -1,14 +1,18 @@
 @echo off
-set /A mobile_points=100
-set /A desktop_points=150
+set /A mobile_points=100*1.5
+set /A desktop_points=150*1.5
 
 set /A points_per_search=5
+
+set /A index=0
+set /A total_points=%mobile_points%+%desktop_points%
 
 set mobile_user_agent="Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
 set desktop_edge_user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.54"
 
-set /A index=0
-set /A total_points=%mobile_points%+%desktop_points%
+echo %points_per_search% points per search
+echo get %mobile_points% points from mobile using the header %mobile_user_agent%
+echo get %desktop_points% points from desktop using the header %desktop_edge_user_agent%
 
 setlocal enabledelayedexpansion
 echo Closing any existing msedge windows bc it will mess with our user-agent headers shenanigans
@@ -18,20 +22,20 @@ taskkill /F /IM msEdge.exe
 set urllist= 
 FOR /L %%X IN (0,%points_per_search%,%mobile_points%) DO set urllist=!urllist! "https://www.bing.com/?q=%%X"
 echo OPENING IN MOBILE MODE
-start msedge --new-window --user-agent=%mobile_user_agent% %urllist%
+start msedge --new-window --disable-application-cache --user-agent=%mobile_user_agent% %urllist%
 
 :wait_for_pages_to_load
-timeout 10
+timeout 30
 taskkill /F /IM msEdge.exe
 
 :open_desktop_urls
 set urllist= 
 FOR /L %%X IN (%mobile_points%,%points_per_search%,%total_points%) DO set urllist=!urllist! "https://www.bing.com/?q=%%X"
 echo OPENING IN DESKTOP MODE
-start msedge --new-window --user-agent=%desktop_edge_user_agent% %urllist% 
+start msedge --new-window --disable-application-cache --user-agent=%desktop_edge_user_agent% %urllist% 
 
 :wait_for_pages_to_load
-timeout 10
+timeout 30
 taskkill /F /IM msEdge.exe
 
 
